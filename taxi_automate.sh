1785b15268d12790ef76c1ba6746d2a749908f33
dev2 <dev2@localhost>
2025-08-04 17:45:21 +0530

    automation files added

---
+++ taxi_automate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bash taxi_hdfs_dir.sh
+bash taxi_put_data.sh
---
+++ taxi_put_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+hdfs dfs -put ~/shared/taxi_data.csv /user/talentum/Final_Project/.
+
+hdfs dfs -ls /user/talentum/Final_Project
